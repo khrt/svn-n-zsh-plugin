d2f70056d40a1a5540854cb73f58fc0628366b6f
Artur <artie@Kenobi.(none)>
2012-12-12 22:28:18 +0200

    implemented base functions #2

--- svn-n.plugin.zsh
+++ svn-n.plugin.zsh
@@ -1,61 +1,64 @@
 
-function svn_prompt_info {
-    if [ $(in_svn) ]; then
-        if [ "x$SVN_SHOW_BRANCH" = "xtrue" ]; then
-            unset SVN_SHOW_BRANCH
-            _DISPLAY=$(svn_get_branch_name)
-        else
-            _DISPLAY=$(svn_get_repo_name)
+function svn_prompt() {
+    if svn_in; then
+        local ref dirty
+        if svn_parse_dirty; then
+            dirty=$ZSH_THEME_SVN_PROMPT_DIRTY
         fi
-        echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_PREFIX\
-$ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR$(svn_dirty)$ZSH_PROMPT_BASE_COLOR"
-        unset _DISPLAY
+        echo -n "$(svn_branch_name) $(svn_rev)$dirty"
     fi
 }
 
-
-function in_svn() {
+function svn_in() {
     if $(svn info >/dev/null 2>&1); then
-        echo 1
+        return 0
+    fi
+    return 1
+}
+
+function svn_working_copy() {
+    if svn_in; then
+        svn info 2> /dev/null | awk -F: '/^Working/ { print $2 }'
     fi
 }
 
-function svn_get_repo_name {
-    if [ $(in_svn) ]; then
-        svn info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
+function svn_parse_dirty() {
+    if svn_in; then
+        if $(svn status $(svn_working_copy) 2> /dev/null | \
+            grep -Eq '^\s*[ACDIM!?L]');
+        then
+            return 0
+        fi
+    fi
+    return 1
+}
 
+function svn_repo_name() {
+    if svn_in; then
+        svn info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
         svn info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
     fi
 }
 
-function svn_get_branch_name {
-    _DISPLAY=$(svn info 2> /dev/null | awk -F/ '/^URL:/ { for (i=0; i<=NF; i++) { if ($i == "branches" || $i == "tags" ) { print $(i+1); break }; if ($i == "trunk") { print $i; break } } }')
-    if [ "x$_DISPLAY" = "x" ]; then
-        svn_get_repo_name
-    else
-        echo $_DISPLAY
+function svn_branch_name() {
+    if svn_in; then
+        svn info 2> /dev/null | \
+            awk -F/ \
+            '/^URL:/ { \
+              for (i=0; i<=NF; i++) { \
+                if ($i == "branches" || $i == "tags" ) { \
+                  print $(i+1); \
+                  break;\
+                }; \
+                if ($i == "trunk") { print $i; break; } \
+              } \
+            }'
     fi
-    unset _DISPLAY
 }
 
-function svn_get_rev_nr {
-    if [ $(in_svn) ]; then
+function svn_rev() {
+    if svn_in; then
         svn info 2> /dev/null | sed -n s/Revision:\ //p
     fi
 }
 
-function svn_dirty_choose {
-    if [ $(in_svn) ]; then
-        if $(svn status `svn info | grep Working | awk -F':' '{ print $2 }'`  2> /dev/null | grep -Eq '^\s*[ACDIM!?L]'); then
-            # Grep exits with 0 when "One or more lines were selected", return "dirty".
-            echo $1
-        else
-            # Otherwise, no lines were found, or an error occurred. Return clean.
-            echo $2
-        fi
-    fi
-}
-
-function svn_dirty {
-    svn_dirty_choose $ZSH_THEME_SVN_PROMPT_DIRTY $ZSH_THEME_SVN_PROMPT_CLEAN
-}
