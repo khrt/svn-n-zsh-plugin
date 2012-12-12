ef818269b57ce1788596e1e1ea64be5b2b0e82aa
Artur <artie@Kenobi.(none)>
2012-12-12 23:23:14 +0200

    cleanup code; wrote the readme file

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-svn-n
-=====
+#SVN-n
+Improved version of SVN plugin provided with the [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh).
 
-oh-my-zsh svn plugin
+###Example
+For `agnoster` zsh theme:
+
+```
+prompt_svn() {
+  if svn_inside; then
+    ZSH_THEME_SVN_PROMPT_DIRTY='±'
+    local ref dirty
+    if svn_parse_dirty; then
+      dirty=$ZSH_THEME_SVN_PROMPT_DIRTY
+      prompt_segment yellow black
+    else
+      prompt_segment green black
+    fi
+    echo -n "⭠ $(svn_branch_name) $(svn_rev)$dirty"
+  fi
+}
+
+...
+
+build_prompt() {
+  RETVAL=$?
+  prompt_status
+  prompt_context
+  prompt_dir
+  prompt_git
+  prompt_svn
+  prompt_end
+}
+```
+
+====
+Based on `oh-my-zsh/plugins/svn.plugin.zsh`.
--- svn-n.plugin.zsh
+++ svn-n.plugin.zsh
@@ -1,64 +1,59 @@
-
+# vim:ft=zsh ts=2 sw=2 sts=2
+#
+#
 function svn_prompt() {
-    if svn_in; then
-        local ref dirty
-        if svn_parse_dirty; then
-            dirty=$ZSH_THEME_SVN_PROMPT_DIRTY
-        fi
-        echo -n "$(svn_branch_name) $(svn_rev)$dirty"
+  if svn_in; then
+    local ref dirty
+    if svn_parse_dirty; then
+      dirty=$ZSH_THEME_SVN_PROMPT_DIRTY
     fi
+    echo -n "$(svn_branch_name) $(svn_rev)$dirty"
+  fi
 }
 
-function svn_in() {
-    if $(svn info >/dev/null 2>&1); then
-        return 0
-    fi
-    return 1
-}
-
-function svn_working_copy() {
-    if svn_in; then
-        svn info 2> /dev/null | awk -F: '/^Working/ { print $2 }'
-    fi
+function svn_is_inside() {
+  if $(svn info >/dev/null 2>&1); then
+    return 0
+  fi
+  return 1
 }
 
 function svn_parse_dirty() {
-    if svn_in; then
-        if $(svn status $(svn_working_copy) 2> /dev/null | \
-            grep -Eq '^\s*[ACDIM!?L]');
-        then
-            return 0
-        fi
+  if svn_is_inside; then
+    root=`svn info 2> /dev/null | sed -n 's/^Working Copy Root Path: //p'`
+    if $(svn status $root 2> /dev/null | grep -Eq '^\s*[ACDIM!?L]'); then
+      return 0
     fi
-    return 1
+  fi
+  return 1
 }
 
 function svn_repo_name() {
-    if svn_in; then
-        svn info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
-        svn info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
-    fi
+  if svn_is_inside; then
+    svn info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
+    svn info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
+  fi
 }
 
 function svn_branch_name() {
-    if svn_in; then
-        svn info 2> /dev/null | \
-            awk -F/ \
-            '/^URL:/ { \
-              for (i=0; i<=NF; i++) { \
-                if ($i == "branches" || $i == "tags" ) { \
-                  print $(i+1); \
-                  break;\
-                }; \
-                if ($i == "trunk") { print $i; break; } \
-              } \
-            }'
-    fi
+  if svn_is_inside; then
+    svn info 2> /dev/null | \
+      awk -F/ \
+      '/^URL:/ { \
+        for (i=0; i<=NF; i++) { \
+          if ($i == "branches" || $i == "tags" ) { \
+            print $(i+1); \
+            break;\
+          }; \
+          if ($i == "trunk") { print $i; break; } \
+        } \
+      }'
+  fi
 }
 
 function svn_rev() {
-    if svn_in; then
-        svn info 2> /dev/null | sed -n s/Revision:\ //p
-    fi
+  if svn_is_inside; then
+    svn info 2> /dev/null | sed -n 's/Revision:\ //p'
+  fi
 }
 
